f592c9cffc6b66268f8eb170bf960628ce4a9420
khkil <khkil@micehub.com>
2021-06-28 20:21:23 +0900

    결과페이지 랭크 동률 제외하고 완성

--- front-end/octagnosis/src/pages/util/ground/DataDetailPage..js
+++ front-end/octagnosis/src/pages/util/ground/DataDetailPage..js
@@ -11,11 +11,12 @@ import TextField from '@material-ui/core/TextField';
 import { useDispatch, useSelector } from 'react-redux';
 import { getQuestionList } from '../../../redux/actions/questionActions';
 import { columnsTotalWidthSelector } from '@material-ui/data-grid';
-import { Box, Button, Chip, Container, Grid } from '@material-ui/core';
+import { Badge, Box, Button, Chip, Container, Grid, Typography } from '@material-ui/core';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import { getGroupList, registGroup } from '../../../redux/actions/groupActions';
 import { getUserAnswers, registUserAnswers } from '../../../redux/actions/userActions';
 import { resultMap } from './DataRegistPage';
+import { X } from 'react-feather';
 
 const StyledTableCell = withStyles((theme) => ({
   head: {
@@ -36,16 +37,113 @@ const StyledTableRow = withStyles((theme) => ({
 }))(TableRow);
 
 const useStyles = makeStyles({
+  root: {
+    maxWidth : "1600px"
+  },
   table: {
     minWidth: 700,
   },
+  rank1: {
+    backgroundColor: "#fdcb4d",
+    fontSize: "20px"
+  },
+  rank2: {
+    backgroundColor: "#cec4c4",
+    fontSize: "20px"
+  }
+  ,rank3: {
+    backgroundColor: "#cd7f32",
+    fontSize: "20px"
+  },
+  resultTable: {
+    margin: "50px",
+    maxWidth: "800px"
+  }
+  
 });
+
+const rows = [
+
+];
+
+const ResultTable = ({ ranks }) => {
+
+  const classes = useStyles();
+  let results = new Object();
+  const grades = [1,2,3];
+  grades.forEach(grade => {
+    let result = [];
+    Object.keys(ranks).forEach(key => {
+      const { ranking }  = ranks[key];
+      ranks[key].resultIdx = key;
+      if(grade === ranking){
+        result = [...result, ranks[key], ];
+      }
+    });
+    results[grade] = result;
+  });
+
+  let maxCount = 0;
+  Object.keys(results).forEach(key => {
+    const count = results[key].length;
+    maxCount = (maxCount < count && count);
+  });
+ 
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.resultTable} aria-label="spanning table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="center" colSpan={3}>
+              최종순위
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            {grades.map((grade, index) => <TableCell key={index} align="center">{`${grade}순위`}</TableCell> )}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {Array.from(Array(maxCount), (e, x) => 
+            <TableRow key={x}>
+              {grades.map((grade, y) => {
+                const result = results[grade];
+                const resultIdx = result[x] ? result[x].resultIdx : null;
+                return (
+                  <TableCell key={y} align="center">
+                    <Typography variant="h6">
+                    {resultIdx && resultMap[resultIdx].title}  
+                    </Typography>
+                  </TableCell> 
+                )
+              })}
+            </TableRow>
+          )}
+{/* 
+          <TableRow>
+            <TableCell align="center">
+              운동형
+            </TableCell>
+            <TableCell align="center">
+              운동형
+            </TableCell>
+            <TableCell align="center">
+              운동형
+            </TableCell>
+          </TableRow>
+           */}
+        
+          
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
 
 const UserInfoInput = ({ dataForm, setDataForm, user }) => {
 
   const dispatch = useDispatch();
   const { userName, userGrade, userEtc } = user;
-  console.log(user);
   useEffect(() => {
     dispatch(getGroupList());
   }, [groupReducer])
@@ -84,7 +182,7 @@ const UserInfoInput = ({ dataForm, setDataForm, user }) => {
               name="user_name" 
               label="이름"  
               margin="normal"
-              disabled={true}
+              readOnly={true}
               value={userName}
               onChange={(e) => {
                 const { name, value } = e.target;
@@ -105,7 +203,7 @@ const UserInfoInput = ({ dataForm, setDataForm, user }) => {
             type="number"
             label="학년"  
             margin="normal"
-            disabled={true}
+            readOnly={true}
             value={userGrade}
             onChange={(e) => {
               const { name, value } = e.target;
@@ -122,7 +220,7 @@ const UserInfoInput = ({ dataForm, setDataForm, user }) => {
             name="user_etc"
             label="반" 
             margin="normal"
-            disabled={true}
+            readOnly={true}
             value={userEtc}
             onChange={(e) => {
               const { name, value } = e.target;
@@ -137,9 +235,7 @@ const UserInfoInput = ({ dataForm, setDataForm, user }) => {
             }}
           />
         </Grid>
-        
       </Grid>
-     
     </Grid>
   );
 }
@@ -149,7 +245,7 @@ const UserInfoInput = ({ dataForm, setDataForm, user }) => {
 const DataDetailPage = ({ history, match }) => {
 
   const classes = useStyles();
-
+  
   const [dataForm, setDataForm] = useState({
     inspection_idx: 3,
     user_info: {},
@@ -161,14 +257,12 @@ const DataDetailPage = ({ history, match }) => {
   const userReducer = useSelector(state => state.userReducer);
 
   useEffect(() => {
-    console.log("useEffect");
     const { user_idx } = match.params;
     dispatch(getUserAnswers(user_idx));
   }, [])
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(history);
     if(confirm("등록하시겠습니까?")){
       dispatch(registUserAnswers(dataForm))
       .then(() => {
@@ -186,6 +280,7 @@ const DataDetailPage = ({ history, match }) => {
     let result = new Object();
     questions.forEach(question => {
       const key = question.result_idx;
+      question.answer_idx = "test";
       if(!result[key]){
         result[key] = [question];
       }else{
@@ -222,16 +317,51 @@ const DataDetailPage = ({ history, match }) => {
     return map;
   }
 
-  
+  const getRank = (questions, answers) => {
+
+    let ranks = {};
+    const answerMap = getAnswer(answers);
+    Object.keys(questions).forEach(key => {
+      const scoreList = questions[key].map(question => {
+        const { question_idx } = question;
+        const score = answerMap[question_idx] ? answerMap[question_idx] : 0;
+        return score;
+      });
+      const totalScore = scoreList.reduce((a, b) => a + b);
+      const info = {
+        totalScore: totalScore,
+      }
+      ranks[key] = info;
+    });
+
+    Object.keys(ranks).map(x => {
+      let rank = ranks[x];
+      const { totalScore } = rank;
+      let ranking = Object.keys(ranks).length + 1;
+      Object.keys(ranks).map(y => { 
+        if(ranks[y].totalScore <= totalScore){
+          ranking--;
+        }
+      })
+      rank.ranking = ranking;
+      return rank;
+    });
+    return ranks;
+  }
+
 
   const { data } = userReducer;
 
   if(!data || !data.questions || !data.answers) return null;
 
-  const { user, questions, answers } = data;
+  const { user, answers } = data;
+  const questions = getQuestions(data.questions);
+  const rank = getRank(questions, answers);
+  
+  
   return (
-    <Container maxWidth="lg">
-      <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit}>
+    <Container maxWidth="lg" className={classes.root}>
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
       
         <UserInfoInput user={data.user} dataForm={dataForm} setDataForm={setDataForm}/>
 
@@ -241,26 +371,25 @@ const DataDetailPage = ({ history, match }) => {
             <TableHead>
               <TableRow>
                 <TableCell align="center" width="10%">-</TableCell>
-                <TableCell align="center">1번</TableCell>
-                <TableCell align="center">2번</TableCell>
-                <TableCell align="center">3번</TableCell>
-                <TableCell align="center">4번</TableCell>
-                <TableCell align="center">5번</TableCell>
-                <TableCell align="center">6번</TableCell>
-                <TableCell align="center">7번</TableCell>
-                <TableCell align="center">8번</TableCell>
-                <TableCell align="center">9번</TableCell>
-                <TableCell align="center">10번</TableCell>
-                <TableCell align="center">11번</TableCell>
-                <TableCell align="center">12번</TableCell>
-                <TableCell align="center">13번</TableCell>
-                <TableCell align="center">14번</TableCell>
-                <TableCell align="center">15번</TableCell>
+                {Object.keys(questions).map((key, x) => {
+                  const question = questions[key];
+                  return (
+                    <TableCell key={x} align="center" width="5%">
+                      {`${x + 1}번`}
+                    </TableCell>
+                  )
+                })}
+                
+                
+                <TableCell align="center" width="5%">총점</TableCell>
+                <TableCell align="center" width="7%">성향 순위</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {Object.keys(getQuestions(questions)).map((key, x) => {
-                const question = getQuestions(questions)[key];
+              {Object.keys(questions).map((key, x) => {
+                
+                const question = questions[key];
+                const { totalScore, ranking } = rank[key];
                 return (
                   <StyledTableRow key={x}>
                     <StyledTableCell align="center" component="th" scope="row">
@@ -271,6 +400,7 @@ const DataDetailPage = ({ history, match }) => {
                     />
                     </StyledTableCell>
                     {question.map((value, y) => {
+                      
                       const answer = getAnswer(answers);
                       const answerValue = answer[value.question_idx];
                       return (
@@ -278,9 +408,9 @@ const DataDetailPage = ({ history, match }) => {
                           <TextField 
                             size="medium" 
                             type="number" 
-                            InputProps={{ inputProps: { min: 1, max: 5 } }} 
+                            InputProps={{ inputProps: { min: 1, max: 5, style: {textAlign: 'center'} } }} 
                             value={answerValue}
-                            disabled={true}
+                            readOnly={true}
                             onChange={(e) => {
                               const { question_idx } = value;
                               const answer_idx = e.target.value;
@@ -290,27 +420,48 @@ const DataDetailPage = ({ history, match }) => {
                         </StyledTableCell>
                       )
                     })}
+                    <StyledTableCell align="center" component="th" scope="row">
+                      <Typography variant="h6" >
+                        {totalScore}
+                      </Typography>
+                    </StyledTableCell>
+                    <StyledTableCell align="center" component="th" scope="row">
+                    
+                      {ranking < 4 ?
+                        <Chip
+                          size="medium"
+                          mr={5}
+                          mb={5}
+                          label={ranking}
+                          className={classes[`rank${ranking}`]}
+                        /> :
+                        <Typography variant="h6" className={classes[`rank${ranking}`]}>
+                          {ranking}
+                        </Typography>
 
-
+                      }
+                     
+                    </StyledTableCell>
                   </StyledTableRow>
                 )
               })}
             </TableBody>
           </Table>
         </TableContainer>
-        <Grid container item xs={12} spacing={3}>
+        <ResultTable ranks={rank}/>
+       {/*  <Grid container item xs={12} spacing={3}>
           <Grid item xs={4}/>
           <Grid item xs={4} style={{textAlign:"center", paddingTop:"50px"}}>
             <Paper className={classes.paper}>
 
-           {/*  <Button variant="contained" color="primary" size="large" type="submit">
+            <Button variant="contained" color="primary" size="large" type="submit">
               등록
             </Button>
- */}
+         
             </Paper>
           </Grid>
           <Grid item xs={4}/>
-        </Grid>
+        </Grid> */}
         
       </form>
     </Container>
