7e4500b1fe06878614ddba408a0ef2e00d3d09cd
khkil <robot625@naver.com>
2024-11-24 22:10:00 +0900

    config: 프록시 타겟 고정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,5 @@
 const {createProxyMiddleware} = require('http-proxy-middleware');
 
-const API_TARGET =
-  process.env.NODE_ENV === 'production'
-    ? 'http://localhost:8088/api' // 운영 환경
-    : 'http://3.39.44.40:8088/api'; // 개발 환경
-
 module.exports = function (app) {
   app.use(
     '/api/v2',
@@ -19,7 +14,8 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: API_TARGET,
+//      target: 'http://3.39.44.40:8088/api',
+      target: 'http://localhost:8088/api',
       changeOrigin: true,
       // pathRewrite를 제거해서 /api 경로 그대로 전달
     })
